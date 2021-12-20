20c80d0d8a8b1b7586e946dc9825e5b135277b02
arvind-gt <arvindrapaka@gmail.com>
2021-12-20 11:21:56 -0800

    Moving the source

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
         .library(name: "cartupEventRecoSDK", targets: ["cartupEventRecoSDK"])
     ],
     targets: [
-        .target(name: "cartupEventRecoSDK", path: "source")
+        .target(name: "cartupEventRecoSDK", path: ["sources", "errors"])
     ]
 )
